afa946ed0e508b11b988e86edcd87dbce187d444
Rob Martin <rob.martin.5758@gmail.com>
2021-03-03 13:27:00 -0500

    added couple links

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from'react';
+import { Link } from "gatsby"
+
+const About = () => {
+    return (
+        <div>
+        <Link to='/'>Go to the Home page</Link>
+        <h1>Welcome to my About page</h1>
+        </div>
+    )
+};
+
+export default About
